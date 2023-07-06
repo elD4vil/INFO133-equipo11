9487df22f6eb8c074e153f5b256e3562b133e1fc
Brancosa <branco.ruiz@alumnos.uach.cl>
2023-07-06 19:47:47 -0400

    A

--- Parte A/insert.py
+++ Parte A/insert.py
@@ -5,8 +5,8 @@ import sys
 # Connect to MariaDB Platform
 try:
     conn = mysql.connector.connect(
-        user="Lulu",
-        password="Calculadora21",
+        user="root",
+        password="123456",
         host="127.0.0.1",
         port=3306
     )
