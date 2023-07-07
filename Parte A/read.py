55e936a9c51b18bd5607a634c7ca0a3a6f57dd64
elSebee <sebastian.anazco@alumnos.uach.cl>
2023-07-07 16:21:35 -0400

    crawling

--- Parte A/read.py
+++ Parte A/read.py
@@ -28,5 +28,25 @@ def ejecutar_consulta(sql):
 
     return resultados
 
+def main():
+    print("OPCIONES")
+    print("XPATH para leer la fecha de un medio (1)")
+    print("Categorias de un medio (2)")
+    print("Medio con la mayor cantidad de fundadores (3)")
+    print("Cual es el medio más viejo (4)")
+    print("Cual es el medio más nuevo (5)")
+
+    opcion = int(input("Ingrese la opción que quiere leer: "))
+
+    # if(opcion == 1):
+    #     dsfsdf
+    
+    # elif(opcion == 2):
+
+    # elif(opcion == 3):
+    
+    # elif(opcion == 4):
+    
+    # elif(opcion == 5):
 
 
